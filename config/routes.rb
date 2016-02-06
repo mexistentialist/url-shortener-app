12d1417e3fc54f2d5b901810571dc29ce83c37b8
salmunoz <munoz.sal3@gmail.com>
2016-02-05 20:10:24 -0600

    added associations and created blank new and create links route

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,10 @@ Rails.application.routes.draw do
    end
   end
 
+  get '/' => 'links#index'
+  get '/links' => 'links#index'
+  get '/links/new' => 'links#new'
+  post '/links' => 'links#create'
+
+
 end
